4f96043709834f37c8be00536917456116a22530
Иван Дьяконов <van7ka.777@gmail.com>
2019-09-08 01:03:46 +0700

    quadfigure disign

--- OOP_LAB_1-2/OOP_LAB_1-2/figure.cpp
+++ OOP_LAB_1-2/OOP_LAB_1-2/figure.cpp
@@ -1,7 +1,37 @@
 #include "figure.h"
 
-class Figure
+int QuadFigure::set_point(int number, int x, int y) 
+{
+}
+int QuadFigure::set_pen_color(int R, int G, int B)
 {
 
-};
+}
+int QuadFigure::set_brush_color(int R, int G, int B)
+{
 
+}
+POINT QuadFigure::get_point(int number) 
+{
+
+}
+COLORREF QuadFigure::get_pen_color()
+{
+
+}
+COLORREF QuadFigure::get_brush_color()
+{
+
+}
+int QuadFigure::move(int x, int y)
+{
+
+}
+int QuadFigure::draw(HDC& hdc)
+{
+
+}
+int QuadFigure::save(char* namefile)
+{
+
+}
--- OOP_LAB_1-2/OOP_LAB_1-2/figure.h
+++ OOP_LAB_1-2/OOP_LAB_1-2/figure.h
@@ -1,4 +1,27 @@
 #pragma once
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <conio.h>
 
-class Figure;
+class QuadFigure
+{
+private:
+	POINT points[4];
+	COLORREF pen_color;
+	COLORREF brush_color;
+public:
+	// set
+	int set_point(int number, int x, int y);
+	int set_pen_color(int R, int G, int B);
+	int set_brush_color(int R, int G, int B);
+	// get
+	POINT get_point(int number);
+	COLORREF get_pen_color();
+	COLORREF get_brush_color();
+	// other
+	int move(int x, int y);
+	int draw(HDC &hdc);
+	int save(char *namefile);
+};
 

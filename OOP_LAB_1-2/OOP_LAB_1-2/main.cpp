ce54972f78547411ed8fa9e50a812dce774ee158
Pickto <van7ka.777@gmail.com>
2019-09-09 18:57:55 +0700

    Update main.cpp

--- OOP_LAB_1-2/OOP_LAB_1-2/main.cpp
+++ OOP_LAB_1-2/OOP_LAB_1-2/main.cpp
@@ -1,42 +1,94 @@
+  
 #include <iostream>
 #include "figure.h"
 #include "utils.h"
 
-int main()
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int main(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-	HWND hwnd = GetConsoleWindow();
-	//HWND hwnd = FindWindow(TEXT("notepad"), TEXT("Áåçûìÿííûé — Áëîêíîò"));
-	HDC hdc = GetDC(hwnd);
-	SetBkColor(hdc, RGB(0, 0, 0));
+	LPCSTR CLASS_NAME = "Sample Window Class";
 
-	QuadFigure fig;
-	QuadFigure other_fig;
+	WNDCLASS wc = { };
 
-	try
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	std::string command;
+	bool isRunning = true;
+
+	while (isRunning)
 	{
-		read_from_file("test.txt", fig);
-		read_from_file("other_test.txt", other_fig);
-		if (fig.is_child(other_fig))
+		std::getline(std::cin, command);
+		if (command == "draw contur")
 		{
-			other_fig.draw_painted(hwnd, hdc);
-			fig.draw_painted(hwnd, hdc);
-		}
-		else if (other_fig.is_child(fig))
-		{
-			fig.draw_painted(hwnd, hdc);
-			other_fig.draw_painted(hwnd, hdc);
-		}
-		else 
-		{
-			std::cout << "eben";
-		}
-	}
-	catch (const char* error)
-	{
-		std::cout << "error: " << error << std::endl;
-	}
+			// Create the window
+			HWND hwnd = CreateWindowEx(
+				0,                              // Optional window styles.
+				CLASS_NAME,                     // Window class
+				"Learn to Program Windows",    // Window text
+				WS_OVERLAPPEDWINDOW,            // Window style
 
-	ReleaseDC(hwnd, hdc);
+				// Size and position
+				CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
+				NULL,       // Parent window    
+				NULL,       // Menu
+				hInstance,  // Instance handle
+				NULL        // Additional application data
+			);
+			ShowWindow(hwnd, nCmdShow);
+
+			QuadFigure fig;
+			read_from_file("test.txt", fig);
+
+			MSG msg = { };
+
+			while (GetMessage(&msg, NULL, 0, 0))
+			{
+				HDC hdc = GetDC(hwnd);
+				try
+				{
+					fig.draw_figuration(hdc, hwnd);
+				}
+				catch (const char* error)
+				{
+					std::cout << "error: " << error << std::endl;
+				}
+				ReleaseDC(hwnd, hdc);
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else if (command == "quit" || command == "q")
+			isRunning = false;
+	}
 	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+
+		// All painting occurs here, between BeginPaint and EndPaint.
+
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }

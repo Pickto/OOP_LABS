8962ea1452b097dccd09c35881ad0b6bcf27c054
Pickto <van7ka.777@gmail.com>
2019-09-18 21:49:41 +0700

    add new menu

--- OOP_LAB_1-2/OOP_LAB_1-2/figure.cpp
+++ OOP_LAB_1-2/OOP_LAB_1-2/figure.cpp
@@ -8,6 +8,7 @@ bool QuadFigure::inside_triangle(POINT a, POINT b, POINT c, POINT check)
 
 	return x1 >= 0 && x2 >= 0 && x3 >= 0 || x1 <= 0 && x2 <= 0 && x3 <= 0;
 }
+
 QuadFigure::QuadFigure()
 {
 	for (int i = 0; i < 4; i++)
@@ -19,6 +20,7 @@ QuadFigure::QuadFigure()
 	pen_style = PS_SOLID;
 	brush_style = -1;
 }
+
 // set 
 void QuadFigure::set_point(int number, int x, int y) 
 {
@@ -28,6 +30,7 @@ void QuadFigure::set_point(int number, int x, int y)
 	points[number].x = x;
 	points[number].y = y;
 } 
+
 void QuadFigure::set_points(POINT *points)
 {
 	if (!QuadFigure::is_convex(points))
@@ -36,6 +39,7 @@ void QuadFigure::set_points(POINT *points)
 	for (int i = 0; i < 4; i++)
 		set_point(i, points[i].x, points[i].y);
 }
+
 void QuadFigure::set_pen_color(int R, int G, int B)
 {
 	if (R > 255 || R < 0 || G > 255 || G < 0 || B > 255 || B < 0)
@@ -43,6 +47,7 @@ void QuadFigure::set_pen_color(int R, int G, int B)
 
 	pen_color = RGB(R, G, B);
 }
+
 void QuadFigure::set_brush_color(int R, int G, int B)
 {
 	if (R > 255 || R < 0 || G > 255 || G < 0 || B > 255 || B < 0)
@@ -50,6 +55,7 @@ void QuadFigure::set_brush_color(int R, int G, int B)
 
 	brush_color = RGB(R, G, B);
 }
+
 void QuadFigure::set_depth_pen(int depth_pen)
 {
 	if (depth_pen < 0)
@@ -57,39 +63,103 @@ void QuadFigure::set_depth_pen(int depth_pen)
 
 	this->depth_pen = depth_pen;
 }
-void QuadFigure::set_pen_style(int pen_style)
-{
-	if(pen_style < PS_SOLID || pen_style > PS_INSIDEFRAME)
-		throw "Invalid pen style";
 
-	this->pen_style = pen_style;
-}
-void QuadFigure::set_brush_style(int brush_style)
+void QuadFigure::set_pen_style(int hash)
 {
-	if ((brush_style < HS_HORIZONTAL || brush_style > HS_DIAGCROSS) && brush_style != -1)
-		throw "Invalid brush style";
+	switch (hash)
+	{
+	case HASH_PS_SOLID:
+		this->pen_style = PS_SOLID;
+		break;
 
-	this->brush_style = brush_style;
+	case HASH_PS_DASH:
+		this->pen_style = PS_DASH;
+		break;
+
+	case HASH_PS_DOT:
+		this->pen_style = PS_DOT;
+		break;
+
+	case HASH_PS_DASHDOT:
+		this->pen_style = PS_DASHDOT;
+		break;
+
+	case HASH_PS_DASHDOTDOT:
+		this->pen_style = PS_DASHDOTDOT;
+		break;
+
+	case HASH_PS_NULL:
+		this->pen_style = PS_NULL;
+		break;
+
+	case HASH_PS_INSIDEFRAME:
+		this->pen_style = PS_INSIDEFRAME;
+		break;
+		
+	default:
+		this->pen_style = PS_SOLID;
+		break;
+	}
 }
+
+void QuadFigure::set_brush_style(int hash)
+{
+	switch (hash)
+	{
+	case HASH_HS_BDIAGONAL:
+		this->brush_style = HS_BDIAGONAL;
+		break;
+
+	case HASH_HS_CROSS:
+		this->brush_style = HS_CROSS;
+		break;
+
+	case HASH_HS_DIAGCROSS:
+		this->brush_style = HS_DIAGCROSS;
+		break;
+
+	case HASH_HS_FDIAGONAL:
+		this->brush_style = HS_FDIAGONAL;
+		break;
+
+	case HASH_HS_HORIZONTAL:
+		this->brush_style = HS_HORIZONTAL;
+		break;
+
+	case HASH_HS_VERTICAL:
+		this->brush_style = HS_VERTICAL;
+		break;
+
+	default:
+		this->brush_style = -1;
+
+	}
+}
+
 // get
 POINT QuadFigure::get_point(int number) 
 {
 	if (number < 0 || number > 3)
 		throw "Out of range";
+
 	return points[number];
 }
+
 COLORREF QuadFigure::get_pen_color()
 {
 	return pen_color;
 }
+
 COLORREF QuadFigure::get_brush_color()
 {
 	return brush_color;
 }
+
 int QuadFigure::get_depth_pen()
 {
 	return depth_pen;
 }
+
 const char* QuadFigure::get_pen_style()
 {
 	switch (pen_style)
@@ -117,8 +187,10 @@ const char* QuadFigure::get_pen_style()
 			
 	default:
 		return "NONE";
+
 	}
 }
+
 const char* QuadFigure::get_brush_style()
 {
 	switch (brush_style)
@@ -143,8 +215,10 @@ const char* QuadFigure::get_brush_style()
 
 	default:
 		return "NONE";
+
 	}
 }
+
 // other
 void QuadFigure::move(int x, int y)
 {
@@ -154,6 +228,7 @@ void QuadFigure::move(int x, int y)
 		points[i].y += y;
 	}
 }
+
 void QuadFigure::draw_figuration(HWND hwnd, HDC hdc)
 {
 	HPEN pen = CreatePen(pen_style, depth_pen, pen_color);
@@ -164,6 +239,7 @@ void QuadFigure::draw_figuration(HWND hwnd, HDC hdc)
 
 	SelectPen(hdc, pen);
 }
+
 void QuadFigure::draw_painted(HWND hwnd, HDC hdc)
 {
 	HBRUSH brush;
@@ -179,6 +255,7 @@ void QuadFigure::draw_painted(HWND hwnd, HDC hdc)
 
 	SelectBrush(hdc, brush);
 }
+
 void QuadFigure::save(std::string namefile)
 {
 	std::ofstream file(namefile);
@@ -196,6 +273,7 @@ void QuadFigure::save(std::string namefile)
 	file << "\nSTYLES\npen " << get_pen_style() << "\n";
 	file << "brush " << get_brush_style();
 }
+
 void QuadFigure::read(std::string namefile)
 {
 	std::ifstream file(namefile);
@@ -245,38 +323,7 @@ void QuadFigure::read(std::string namefile)
 			{
 				file >> read;
 
-				read_hash = hash_string(read);
-
-				switch (read_hash)
-				{
-				case HASH_PS_SOLID:
-					set_pen_style(PS_SOLID);
-					break;
-
-				case HASH_PS_DASH:
-					set_pen_style(PS_DASH);
-					break;
-
-				case HASH_PS_DOT:
-					set_pen_style(PS_DOT);
-					break;
-
-				case HASH_PS_DASHDOT:
-					set_pen_style(PS_DASHDOT);
-					break;
-
-				case HASH_PS_DASHDOTDOT:
-					set_pen_style(PS_DASHDOTDOT);
-					break;
-
-				case HASH_PS_NULL:
-					set_pen_style(PS_NULL);
-					break;
-
-				case HASH_PS_INSIDEFRAME:
-					set_pen_style(PS_INSIDEFRAME);
-					break;
-				}
+				set_pen_style(hash_string(read));
 			}
 
 			file >> read;
@@ -285,34 +332,7 @@ void QuadFigure::read(std::string namefile)
 			{
 				file >> read;
 
-				read_hash = hash_string(read);
-
-				switch (read_hash)
-				{
-				case HASH_HS_BDIAGONAL:
-					set_brush_style(HS_BDIAGONAL);
-					break;
-
-				case HASH_HS_CROSS:
-					set_brush_style(HS_CROSS);
-					break;
-
-				case HASH_HS_DIAGCROSS:
-					set_brush_style(HS_DIAGCROSS);
-					break;
-
-				case HASH_HS_FDIAGONAL:
-					set_brush_style(HS_FDIAGONAL);
-					break;
-
-				case HASH_HS_HORIZONTAL:
-					set_brush_style(HS_HORIZONTAL);
-					break;
-
-				case HASH_HS_VERTICAL:
-					set_brush_style(HS_VERTICAL);
-					break;
-				}
+				set_brush_style(hash_string(read));
 			}
 			break;
 		}
@@ -320,14 +340,17 @@ void QuadFigure::read(std::string namefile)
 
 	file.close();
 }
+
 bool QuadFigure::is_child(QuadFigure& other_fig)
 {
 	for (int i = 0; i < 4; i++)
 		if (!QuadFigure::inside_triangle(other_fig.points[0], other_fig.points[1], other_fig.points[2], points[i]) &&
 			!QuadFigure::inside_triangle(other_fig.points[2], other_fig.points[3], other_fig.points[0], points[i]))
 			return false;
+
 	return true;
 }
+
 bool QuadFigure::in_rect_window(HWND hwnd)
 {
 	RECT size_window;
@@ -336,12 +359,15 @@ bool QuadFigure::in_rect_window(HWND hwnd)
 	for (int i = 0; i < 4; i++)
 		if (points[i].x > size_window.right || points[i].y > size_window.bottom || points[i].x < 0 || points[i].y < 0)
 			return false;
+
 	return true;
 }
+
 bool QuadFigure::is_convex(POINT* points)
 {
 	for (int i = 0; i < 4; i++)
 		if (QuadFigure::inside_triangle(points[(i + 1) % 4], points[(i + 2) % 4], points[(i + 3) % 4], points[i]))
 			return false;
+
 	return true;
 }
--- OOP_LAB_1-2/OOP_LAB_1-2/main.cpp
+++ OOP_LAB_1-2/OOP_LAB_1-2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "figure.h"
+#include "hash.h"
 
 
 void command_handler(QuadFigure &fig, HWND &hwnd, HDC &hdc)
@@ -33,13 +34,60 @@ void command_handler(QuadFigure &fig, HWND &hwnd, HDC &hdc)
 		std::cin >> n;
 		std::cout << fig.get_point(n).x << " " << fig.get_point(n).y << "\n";
 	}
+	else if (command == "set_points")
+	{
+		int x, y;
+		std::string read;
+		POINT points[4];
+		for (int i = 0; i < 4; i++)
+		{
+			std::cin >> x >> read >> y;
+			points[i].x = x;
+			points[i].y = y;
+		}
+		fig.set_points(points);
+	}
+	else if (command == "move")
+	{
+		int x, y;
+		std::string read;
+		std::cin >> x >> read >> y;
+		fig.move(x, y);
+	}
+	else if (command == "set_brush_style")
+	{
+		std::string style;
+		std::cin >> style;
+		fig.set_brush_style(hash_string(style));
+	}
+	else if (command == "set_brush_color")
+	{
+		int R, G, B;
+		std::string read;
+		std::cin >> R >> read >> G >> read >> B;
+		fig.set_brush_color(R, G, B);
+	}
+	else if (command == "set_pen_style")
+	{
+		std::string style;
+		std::cin >> style;
+		fig.set_pen_style(hash_string(style));
+	}
+	else if (command == "set_pen_color")
+	{
+		int R, G, B;
+		std::string read;
+		std::cin >> R >> read >> G >> read >> B;
+		fig.set_pen_color(R, G, B);
+	}
 	else
 		throw "Unknown command";
 }
 
 int main()
 {
-	HWND hwnd = GetConsoleWindow();
+	//HWND hwnd = GetConsoleWindow();
+	HWND hwnd = FindWindow(TEXT("notepad"), TEXT("Безымянный — Блокнот"));
 	HDC hdc = GetDC(hwnd);
 
 	QuadFigure A;
